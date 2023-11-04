579f85bf98116ec2952442cd54a7c0f8e9d6d98c
wjdghks963 <chsw000@naver.com>
2023-11-04 22:55:54 +0900

    added : - svg 추가 - font 추가 - global style 추가

---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const appBarStyle = TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const infoTextStyle = TextStyle(
+      fontSize: 14,
+      color: Colors.black,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500);
+}
+
+class ColorStyles {
+  static const Color darkGray = Color(0xff494949);
+}
+
+class BgColor {
+  static const Color mainColor = Color(0xFFBFD9CA);
+}
