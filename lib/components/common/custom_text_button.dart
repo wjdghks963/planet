1e4e928d48688d28c52374801d90ce553454c478
wjdghks963 <chsw000@naver.com>
2023-11-17 21:00:17 +0900

    added : - 배경색 설정 가능

--- lib/components/common/custom_text_button.dart
+++ lib/components/common/custom_text_button.dart
@@ -3,11 +3,13 @@ import 'package:planet/theme.dart';
 
 class CustomTextButton extends StatelessWidget {
   String content;
+  Color backgroundColor;
   Function onPressed;
 
   CustomTextButton({
     super.key,
     required this.content,
+    required this.backgroundColor,
     required this.onPressed,
   });
 
@@ -15,12 +17,12 @@ class CustomTextButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       style: TextButton.styleFrom(
-        padding: EdgeInsets.symmetric(vertical: 10.0),
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20.0),
         ),
-        backgroundColor: ColorStyles.mainAccent,
-        minimumSize: Size(double.infinity, 0),
+        backgroundColor: backgroundColor,
+        minimumSize: const Size(double.infinity, 0),
       ),
       onPressed: () => onPressed(),
       child: Text(
