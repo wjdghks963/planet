92fd6da4f9447af3227fd34f93e6f256543e416c
wjdghks963 <chsw000@naver.com>
2023-11-17 14:10:02 +0900

    added : - 선택한 plant에 대한 필드 필요에 의해 추가

--- lib/services/selected_plant_detail_service.dart
+++ lib/services/selected_plant_detail_service.dart
@@ -1,8 +1,19 @@
 import 'package:planet/models/selected_plant_detail_model.dart';
 
-class SelectedPlantDetailService  {
+class SelectedPlantDetailService {
   SelectedPlantDetailModel selectedDetail(
-      {required int uid, required String nickName, String? imgUrl}) {
-    return SelectedPlantDetailModel(uid: uid, nickName: nickName, imgUrl: imgUrl);
+      {required int plantId,
+      required String nickName,
+      String? imgUrl,
+      String? scientificName,
+      int? heartCount,
+      bool? hearted}) {
+    return SelectedPlantDetailModel(
+        plantId: plantId,
+        nickName: nickName,
+        imgUrl: imgUrl,
+        scientificName: scientificName,
+        heartCount: heartCount,
+        hearted: hearted);
   }
 }
