784e072586de8a3cbeeadc368939444f4908eef9
wjdghks963 <chsw000@naver.com>
2023-11-17 21:39:23 +0900

    added : - 신고 버튼

---
+++ lib/components/common/report_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:planet/components/common/custom_alert_dialog.dart';
+import 'package:planet/components/common/custom_select_dialog.dart';
+
+class ReportButton extends StatelessWidget {
+  void reportPlant() async {
+    final result = await Get.dialog<bool>(const CustomSelectDialog(
+      title: "신고",
+      content:
+          "이 식물의 이미지나 설명이 부적절하다고 생각하시나요?\n\n부적절한 내용은 커뮤니티에 해를 끼치며, 신고된 내용은 우리 팀에서 신중하게 검토됩니다.\n\n실제로 부적절한 경우, 해당 내용은 삭제될 수 있습니다.\n\n정말로 이 식물을 신고하시겠습니까?",
+    ));
+
+    if (result != null && result) {
+      Get.dialog(CustomAlertDialog(alertContent: "신고 완료되었습니다."));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Icon(Icons.report, color: Colors.redAccent),
+        ),
+        onTap: () {
+          reportPlant();
+        });
+  }
+}
