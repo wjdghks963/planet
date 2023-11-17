24e184c0b746591e45305d09a589d085deacf6ab
wjdghks963 <chsw000@naver.com>
2023-11-17 21:40:44 +0900

    added : - access_token 만료시 refresh_token로 재발급 하는 로직 추가

---
+++ lib/services/auth_api_service.dart
@@ -0,0 +1,36 @@
+import 'package:get/get_connect/connect.dart';
+import 'package:planet/utils/OAuth/token_storage.dart';
+import 'package:planet/utils/develop_domain.dart';
+
+class AuthApiService extends GetConnect {
+  final storage = TokenStorage();
+  String domain = DevelopDomain().run();
+
+  Future<bool> refreshToken() async {
+    try {
+      String? accessToken = await storage.getToken(TokenType.acc);
+      String? refreshToken = await storage.getToken(TokenType.refresh);
+
+      if (refreshToken == null) {
+        return false;
+      }
+      String authorizationHeader = 'Bearer $accessToken';
+      final response = await get(
+        '$domain/refresh',
+        headers: {'Authorization': authorizationHeader},
+        query: {'refreshToken': refreshToken},
+      );
+
+      if (response.statusCode == 200) {
+        await storage.saveToken(
+            response.body['access_token'], response.body['refresh_token']);
+
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print("Error refreshing token: $e");
+      return false;
+    }
+  }
+}
