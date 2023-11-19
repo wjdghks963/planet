ad9d5d6060260fffc330b74ac93a38721b4092fe
wjdghks963 <chsw000@naver.com>
2023-11-19 20:16:17 +0900

    added : - 로딩 중 로띠 추가

--- lib/screen/plant/add_plant_form.dart
+++ lib/screen/plant/add_plant_form.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
 import 'package:planet/components/common/CustomAppBar.dart';
 import 'package:planet/components/common/custom_alert_dialog.dart';
 import 'package:planet/components/common/custom_text_button.dart';
 import 'package:planet/components/common/form_textfield.dart';
-import 'package:planet/controllers/plant_controller.dart';
+import 'package:planet/controllers/plant/plant_controller.dart';
 import 'package:planet/models/api/plant/plant_form_model.dart';
-import 'package:planet/services/api_manager.dart';
 import 'package:planet/services/plant_api_service.dart';
 import 'package:planet/theme.dart';
 import 'package:planet/utils/image_resizer.dart';
@@ -23,6 +23,7 @@ class AddPlantForm extends StatefulWidget {
 }
 
 class _AddPlantFormState extends State<AddPlantForm> {
+  late PlantController plantController;
   late TextEditingController nickNameController;
   late TextEditingController scientificNameController;
 
@@ -58,20 +59,12 @@ class _AddPlantFormState extends State<AddPlantForm> {
       compressedData!,
     );
 
-    try {
-      await PlantsApiClient().addPlant(newPlant);
-
-      final plantController = Get.find<PlantController>();
-      plantController.fetchPlants();
-      Get.back();
-    } catch (e) {
-      await Get.dialog(CustomAlertDialog(alertContent: e.toString()));
-      Get.back();
-    }
+    await plantController.addNewPlant(newPlant);
   }
 
   @override
   void initState() {
+    plantController = Get.find<PlantController>();
     nickNameController = TextEditingController();
     scientificNameController = TextEditingController();
     super.initState();
@@ -90,63 +83,73 @@ class _AddPlantFormState extends State<AddPlantForm> {
         appBar: CustomAppBar(
           title: "식물 추가",
         ),
-        body: Container(
-          color: BgColor.mainColor,
-          child: ListView(
-            padding:
+        body: Obx(() {
+          if (plantController.isLoading == true) {
+            return Center(
+                child: Lottie.asset('assets/lotties/loading_lottie.json'));
+          } else {
+            return Container(
+              color: BgColor.mainColor,
+              child: ListView(
+                padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            children: [
-              Column(
                 children: [
-                  FormTextField(
-                      controller: nickNameController,
-                      hintText: "별명",
-                      labelText: "이름 (별명)"),
-                  const SizedBox(
-                    height: 20,
+                  Column(
+                    children: [
+                      FormTextField(
+                          controller: nickNameController,
+                          hintText: "별명",
+                          labelText: "이름 (별명)"),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormTextField(
+                          controller: scientificNameController,
+                          hintText: "정확하지 않아도 괜찮습니다.",
+                          labelText: "식물 학명"),
+                    ],
                   ),
-                  FormTextField(
-                      controller: scientificNameController,
-                      hintText: "정확하지 않아도 괜찮습니다.",
-                      labelText: "식물 학명"),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                      child: _pickedImage != null
+                          ? InkWell(
+                        onTap: () => getImage(ImageSource.gallery),
+                        child: Image.file(
+                          File(_pickedImage!.path),
+                          fit: BoxFit.fitWidth,
+                          height: 300,
+                        ),
+                      )
+                          : InkWell(
+                        onTap: () => getImage(ImageSource.gallery),
+                        child: Container(
+                          color: Colors.white,
+                          height: 300,
+                          padding: EdgeInsets.all(100),
+                          child: SvgPicture.asset('assets/icons/image.svg'),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  CustomTextButton(
+                      content: "작성 완료",
+                      backgroundColor: ColorStyles.mainAccent,
+                      onPressed: () {
+                        submitPlant();
+                      }),
                 ],
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              ClipRRect(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(20.0),
-                  ),
-                  child: _pickedImage != null
-                      ? InkWell(
-                          onTap: () => getImage(ImageSource.gallery),
-                          child: Image.file(
-                            File(_pickedImage!.path),
-                            fit: BoxFit.fitWidth,
-                            height: 300,
-                          ),
-                        )
-                      : InkWell(
-                          onTap: () => getImage(ImageSource.gallery),
-                          child: Container(
-                            color: Colors.white,
-                            height: 300,
-                            padding: EdgeInsets.all(100),
-                            child: SvgPicture.asset('assets/icons/image.svg'),
-                          ),
-                        )),
-              const SizedBox(
-                height: 50,
-              ),
-              CustomTextButton(
-                  content: "작성 완료",
-                  backgroundColor: ColorStyles.mainAccent,
-                  onPressed: () {
-                    submitPlant();
-                  }),
-            ],
-          ),
-        ));
+            );
+          }
+        })
+
+
+    );
   }
 }
