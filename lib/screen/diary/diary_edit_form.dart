5bfb7e64cc36ddfcde644dd0ebde5e44d1600dc0
wjdghks963 <chsw000@naver.com>
2023-11-20 20:35:19 +0900

    added : - diary controller - diary CRUD

--- lib/screen/diary/diary_edit_form.dart
+++ lib/screen/diary/diary_edit_form.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
 import 'package:planet/components/common/CustomAppBar.dart';
 import 'package:planet/components/common/custom_alert_dialog.dart';
+import 'package:planet/components/common/custom_select_dialog.dart';
 import 'package:planet/components/common/custom_text_button.dart';
-import 'package:planet/controllers/selected_plant_detail_controller.dart';
+import 'package:planet/controllers/diary_controller.dart';
+import 'package:planet/controllers/plant/selected_plant_detail_controller.dart';
 import 'package:planet/models/api/diary/diary_detail_model.dart';
 import 'package:planet/models/api/diary/diary_form_dto.dart';
 import 'package:planet/services/diary_api_service.dart';
@@ -22,6 +25,7 @@ class DiaryEditForm extends StatefulWidget {
 }
 
 class _DiaryEditFormState extends State<DiaryEditForm> {
+  late DiaryController diaryController;
   final DiaryDetailModel diaryDetail = Get.arguments["diary"];
 
   final TextEditingController _contentController = TextEditingController();
@@ -43,10 +47,19 @@ class _DiaryEditFormState extends State<DiaryEditForm> {
     SelectedPlantDetailController selectedPlantDetailController =
         Get.find<SelectedPlantDetailController>();
 
+    String content = _contentController.text;
+
+    if (_pickedImage == null) {
+      Get.dialog(CustomAlertDialog(alertContent: "사진을 등록해 주세요."));
+      return;
+    }
+    if (content == "" ) {
+      Get.dialog(CustomAlertDialog(alertContent: "빈칸을 전부 채워 주세요."));
+      return;
+    }
+
     // 이미지 압축
     String? compressedData = await compressImage(_pickedImage!);
-
-    DiaryApiClient apiDiaryForm = DiaryApiClient();
 
     DiaryFormDTO diaryFormDTO = DiaryFormDTO(
         plantId: selectedPlantDetailController.selectedPlant.plantId!,
@@ -55,28 +68,20 @@ class _DiaryEditFormState extends State<DiaryEditForm> {
         isPublic: isPublic,
         createdAt: Get.arguments["date"]);
 
-    try {
-      await apiDiaryForm.editForm(diaryDetail.id, diaryFormDTO);
-      return Get.back();
-    } catch (e) {
-      Get.dialog(CustomAlertDialog(alertContent: e.toString()));
-    }
+    await diaryController.editDiary(diaryDetail.id, diaryFormDTO);
   }
 
   Future<void> removeDiary() async {
-    DiaryApiClient apiDiaryForm = DiaryApiClient();
-
-    try {
-      await apiDiaryForm.removeDiary(diaryDetail.id);
-
-      return Get.back();
-    } catch (e) {
-      Get.dialog(CustomAlertDialog(alertContent: e.toString()));
+    var result = await Get.dialog(const CustomSelectDialog(
+        title: "삭제", content: "관련된 모든 데이터가 삭제되고 복구할 수 없습니다."));
+    if (result == true) {
+      await diaryController.removeDiary(diaryDetail.id);
     }
   }
 
   @override
   void initState() {
+    diaryController = Get.find<DiaryController>();
     _contentController.text = diaryDetail.content;
 
     super.initState();
@@ -89,119 +94,128 @@ class _DiaryEditFormState extends State<DiaryEditForm> {
 
     return Scaffold(
         appBar: CustomAppBar(
-          title: "일지 작성",
+          title: "일지 수정",
         ),
-        body: Container(
-          color: BgColor.mainColor,
-          child: ListView(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Obx(() {
+          if (diaryController.isLoading == true) {
+            return Center(
+                child: Lottie.asset('assets/lotties/loading_lottie.json'));
+          } else {
+            return Container(
+              color: BgColor.mainColor,
+              child: ListView(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 10.0),
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        selectedPlantDetailController.selectedPlant.nickName ??
-                            "",
-                        style: TextStyles.whiteTitleStyle,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            selectedPlantDetailController
+                                    .selectedPlant.nickName ??
+                                "",
+                            style: TextStyles.whiteTitleStyle,
+                          ),
+                          Text(
+                            Get.arguments?["date"] ?? "",
+                            style: TextStyles.whiteTitleStyle,
+                          ),
+                        ],
                       ),
-                      Text(
-                        Get.arguments["date"] ?? "",
-                        style: TextStyles.whiteTitleStyle,
-                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "공개",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          Switch(
+                              value: isPublic,
+                              activeColor: ColorStyles.mainAccent,
+                              onChanged: (value) {
+                                setState(() {
+                                  isPublic = value;
+                                });
+                              })
+                        ],
+                      )
                     ],
                   ),
-                  Column(
-                    children: [
-                      const Text(
-                        "공개",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20.0),
                       ),
-                      Switch(
-                          value: isPublic,
-                          activeColor: ColorStyles.mainAccent,
-                          onChanged: (value) {
-                            setState(() {
-                              isPublic = value;
-                            });
-                          })
-                    ],
-                  )
+                      child: _pickedImage != null
+                          ? InkWell(
+                              onTap: () => getImage(ImageSource.gallery),
+                              child: Image.file(
+                                File(_pickedImage!.path),
+                                fit: BoxFit.fitWidth,
+                                height: 300,
+                              ),
+                            )
+                          : InkWell(
+                              onTap: () => getImage(ImageSource.gallery),
+                              child: Container(
+                                color: Colors.white,
+                                height: 300,
+                                padding: EdgeInsets.all(100),
+                                child:
+                                    SvgPicture.asset('assets/icons/image.svg'),
+                              ),
+                            )),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    height: 400,
+                    child: TextField(
+                      controller: _contentController,
+                      maxLines: 200,
+                      keyboardType: TextInputType.multiline,
+                      decoration: InputDecoration(
+                        hintText: "기록하고 싶은 내용을 입력하세요.",
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide:
+                              const BorderSide(color: ColorStyles.mainAccent),
+                        ),
+                      ),
+                      style: const TextStyle(height: 1.5),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  CustomTextButton(
+                      content: "작성 완료",
+                      backgroundColor: ColorStyles.mainAccent,
+                      onPressed: () {
+                        editForm();
+                      }),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  CustomTextButton(
+                      content: "삭제",
+                      backgroundColor: Colors.red,
+                      onPressed: () {
+                        removeDiary();
+                      }),
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              ClipRRect(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(20.0),
-                  ),
-                  child: _pickedImage != null
-                      ? InkWell(
-                          onTap: () => getImage(ImageSource.gallery),
-                          child: Image.file(
-                            File(_pickedImage!.path),
-                            fit: BoxFit.fitWidth,
-                            height: 300,
-                          ),
-                        )
-                      : InkWell(
-                          onTap: () => getImage(ImageSource.gallery),
-                          child: Container(
-                            color: Colors.white,
-                            height: 300,
-                            padding: EdgeInsets.all(100),
-                            child: SvgPicture.asset('assets/icons/image.svg'),
-                          ),
-                        )),
-              const SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                height: 400,
-                child: TextField(
-                  controller: _contentController,
-                  maxLines: 200,
-                  keyboardType: TextInputType.multiline,
-                  decoration: InputDecoration(
-                    hintText: "기록하고 싶은 내용을 입력하세요.",
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                      borderSide:
-                          const BorderSide(color: ColorStyles.mainAccent),
-                    ),
-                  ),
-                  style: const TextStyle(height: 1.5),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomTextButton(
-                  content: "작성 완료",
-                  backgroundColor: ColorStyles.mainAccent,
-                  onPressed: () {
-                    editForm();
-                  }),
-              const SizedBox(
-                height: 10.0,
-              ),
-              CustomTextButton(
-                  content: "삭제",
-                  backgroundColor: Colors.red,
-                  onPressed: () {
-                    removeDiary();
-                  }),
-            ],
-          ),
-        ));
+            );
+          }
+        }));
   }
 }
