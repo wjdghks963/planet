bf4ec68190d768d21b432a627cbc80ee593e2681
wjdghks963 <chsw000@naver.com>
2023-11-24 17:56:09 +0900

    added : - 500대 에러

--- lib/models/api/exception/ServerException.dart
+++ lib/models/api/exception/ServerException.dart
@@ -3,10 +3,16 @@ import 'dart:convert';
 class ServerException implements Exception {
   final String message;
 
+  ServerException(this.message);
+
   ServerException.fromResponse(dynamic response) :
         message = _parseErrorMessage(response);
 
   static String _parseErrorMessage(dynamic response) {
+   if(response == null){
+     return "서버 에러";
+   }
+
     try {
       final Map<String, dynamic> responseData = response is String
           ? json.decode(response)
