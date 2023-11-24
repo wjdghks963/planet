2073e67cf1f5392592f01a4f35e923bc1f81a1fb
wjdghks963 <chsw000@naver.com>
2023-11-24 17:57:36 +0900

    modified : - 리프레시 토큰 받는 uri 수정

--- lib/services/auth_api_service.dart
+++ lib/services/auth_api_service.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_connect/connect.dart';
 import 'package:planet/main.dart';
 import 'package:planet/utils/OAuth/token_storage.dart';
 import 'package:planet/utils/develop_domain.dart';
@@ -10,28 +9,23 @@ class AuthApiService extends GetConnect {
 
   Future<bool> refreshToken() async {
     try {
-      String? accessToken = await storage.getToken(TokenType.acc);
       String? refreshToken = await storage.getToken(TokenType.refresh);
 
       if (refreshToken == null) {
         return false;
       }
-      String authorizationHeader = 'Bearer $accessToken';
-      final response = await get(
-        '$domain/refresh',
-        headers: {'Authorization': authorizationHeader},
+      final response = await post(
+        '$domain/users/refresh',
+        {},
         query: {'refreshToken': refreshToken},
       );
 
       if (response.statusCode == 200) {
         await storage.saveToken(
             response.body['access_token'], response.body['refresh_token']);
-
         return true;
       }
-      if (response.statusCode == 403) {
-        Get.offAll(() => const HomeScreen());
-      }
+
       return false;
     } catch (e) {
       print("Error refreshing token: $e");
