1e7dd2e6a59af94ae71510fa53652360de74c345
wjdghks963 <chsw000@naver.com>
2023-11-29 13:27:06 +0900

    refactor : - 함수 build 밖 관리 - 새로운 UI 적용

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,39 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:planet/components/home/random_plants_carousel.dart';
-import 'package:planet/components/home/recent_plants_carousel.dart';
-import 'package:planet/screen/plant/recent_plants_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:planet/components/common/component_title.dart';
+import 'package:planet/components/common/custom_ad_widget.dart';
+import 'package:planet/components/common/more_button.dart';
+import 'package:planet/components/home/plants_carousel.dart';
+import 'package:planet/components/home/plants_grid.dart';
+import 'package:planet/controllers/plant/pagenation_plants_controller.dart';
+import 'package:planet/controllers/plant/plant_controller.dart';
+import 'package:planet/screen/plant/type_plants_screen.dart';
 import 'package:planet/theme.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void goToPopular() {
+    Get.to(
+      () => const TypePlantsScreen(plantType: PlantType.popular),
+      transition: Transition.rightToLeft,
+    );
+  }
+
+  void goToRecent() {
+    Get.to(
+      () => const TypePlantsScreen(plantType: PlantType.recent),
+      transition: Transition.rightToLeft,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final recentPlantsController = Get.put(RecentPlantsController());
+    final popularPlantsController = Get.put(PopularPlantsController());
+    final randomPlantsController = Get.put(RandomPlantsController());
+
     return Container(
         width: double.infinity,
         color: BgColor.mainColor,
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: ListView(
           children: [
+            const CustomAdWidget(),
             const SizedBox(
               height: 25.0,
             ),
             Column(
-              children: const [
-                SizedBox(
+              children: [
+                 SizedBox(
                     width: double.infinity,
-                    child: Text("랜덤",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ))),
-                SizedBox(
+                    child: ComponentTitle("랜덤")),
+                const SizedBox(
                   height: 20.0,
                 ),
-                RandomPlantsCarousel(),
+                Obx(() {
+                  if (randomPlantsController.isLoading.value) {
+                    return Center(
+                        child:
+                            Lottie.asset('assets/lotties/loading_lottie.json'));
+                  } else {
+                    return PlantsCarousel(
+                        plants: randomPlantsController.randomPlants);
+                  }
+                })
               ],
             ),
             const SizedBox(
@@ -46,24 +84,59 @@ class HomeScreen extends StatelessWidget {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Text("최신",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        InkWell(
-                          child: const Text("더 보기",
-                              textAlign: TextAlign.left,
-                              style: TextStyles.whiteLabelTextStyle),
-                          onTap: () {
-                            Get.to(() => const  RecentPlantsScreen());
-                          },
-                        )
+                       ComponentTitle("최신"),
+                        MoreButton(
+                          onPressed: goToRecent,
+                        ),
                       ],
                     )),
-                const RecentPlantsCarousel()
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Obx(() {
+                  if (recentPlantsController.isFetching.value) {
+                    return Center(
+                        child:
+                            Lottie.asset('assets/lotties/loading_lottie.json'));
+                  } else {
+                    return PlantsGrid(
+                        plants: recentPlantsController.recentPlants);
+                  }
+                })
+              ],
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
+            Column(
+              children: [
+                SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ComponentTitle("인기"),
+                        MoreButton(
+                          onPressed: goToPopular,
+                        ),
+                      ],
+                    )),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Obx(() {
+                  if (popularPlantsController.isFetching.value) {
+                    return Center(
+                        child:
+                            Lottie.asset('assets/lotties/loading_lottie.json'));
+                  } else {
+                    return PlantsGrid(
+                        plants: popularPlantsController.popularPlants);
+                  }
+                }),
+                const SizedBox(
+                  height: 20.0,
+                )
               ],
             ),
           ],
