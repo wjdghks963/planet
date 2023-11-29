216afed2411d8410eb55bc8941102d2ce3b94ffb
wjdghks963 <chsw000@naver.com>
2023-11-29 13:22:08 +0900

    refactor : - 옵션 주석 삭제 및 케로셀의 자식들에게 key값 부여

--- lib/components/home/plants_carousel.dart
+++ lib/components/home/plants_carousel.dart
@@ -1,87 +1,49 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:planet/components/home/plant_info_card.dart';
 import 'package:planet/models/api/plant/plant_summary_model.dart';
 
-class PlantsCarousel extends StatefulWidget {
-  final List<PlantSummaryModel> plants;
+class PlantsCarousel extends StatelessWidget {
+  List<PlantSummaryModel> plants;
 
-  const PlantsCarousel({super.key, required this.plants});
-
-  @override
-  State<PlantsCarousel> createState() => _PlantsCarouselState();
-}
-
-class _PlantsCarouselState extends State<PlantsCarousel> {
-  CarouselController carouselController = CarouselController();
+  PlantsCarousel({super.key, required this.plants});
 
   @override
   Widget build(BuildContext context) {
+    CarouselController carouselController = CarouselController();
 
-    return Obx(() => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CarouselSlider(
-              carouselController: carouselController,
-              items: widget.plants.map((plant) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return PlantInfoCard(
-                      imgUrl: plant.imgUrl,
-                      nickName: plant.nickName,
-                      heart: plant.heartCount,
-                      plantId: plant.plantId,
-                    );
-                  },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CarouselSlider(
+          key: UniqueKey(),
+          carouselController: carouselController,
+          items: plants.map((plant) {
+            return Builder(
+              builder: (BuildContext context) {
+                return PlantInfoCard(
+                  imgUrl: plant.imgUrl ?? "",
+                  nickName: plant.nickName ?? "",
+                  heart: plant.heartCount ?? 0,
+                  plantId: plant.plantId ?? 1,
                 );
-              }).toList(),
-              options: CarouselOptions(
-                height: 250,
-
-// Set the size of each carousel item
-// if height is not specified
-                aspectRatio: 16 / 9,
-
-// Set how much space current item widget
-// will occupy from current page view
-                viewportFraction: 0.8,
-
-// Set the initial page
-                initialPage: 0,
-
-// Set carousel to repeat when reaching the end
-                enableInfiniteScroll: true,
-
-// Set carousel to scroll in opposite direction
-                reverse: false,
-
-// Set carousel to display next page automatically
-                autoPlay: true,
-
-// Set the duration of which carousel slider will wait
-// in current page utill it moves on to the next
-                autoPlayInterval: const Duration(seconds: 5),
-
-// Set the duration of carousel slider
-// scrolling to the next page
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
-
-// Set the carousel slider animation
-                autoPlayCurve: Curves.fastOutSlowIn,
-
-// Set the current page to be displayed
-// bigger than previous or next page
-                enlargeCenterPage: true,
-
-// Do actions for each page change
-                onPageChanged: (index, reason) {},
-
-// Set the scroll direction
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-          ],
-        ));
+              },
+            );
+          }).toList(),
+          options: CarouselOptions(
+            height: 250,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.8,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 5),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            scrollDirection: Axis.horizontal,
+          ),
+        ),
+      ],
+    );
   }
 }
