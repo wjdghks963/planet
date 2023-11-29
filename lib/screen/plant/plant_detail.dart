c5472de10f5edf18c28a0016e4534989ed190cac
wjdghks963 <chsw000@naver.com>
2023-11-29 13:24:18 +0900

    refactor : - 빌드 시에 필요한 컨트롤러 찾아 api 통신 가능하도록 함 - responsive 실행 함수들 build 밖에서 관리

--- lib/screen/plant/plant_detail.dart
+++ lib/screen/plant/plant_detail.dart
@@ -8,90 +8,102 @@ import 'package:planet/components/custom_calendar.dart';
 import 'package:planet/components/common/CustomAppBar.dart';
 import 'package:planet/controllers/plant/plant_detail_controller.dart';
 import 'package:planet/controllers/plant/selected_plant_detail_controller.dart';
+import 'package:planet/screen/ai/ai_chat_screen.dart';
 import 'package:planet/screen/plant/edit_plant_form.dart';
 import 'package:planet/theme.dart';
 
-// TODO:: 내거면 수정 가능 아니면 svg도 안뜨게
 class PlantDetail extends StatefulWidget {
-  const PlantDetail({super.key});
+  PlantDetail({super.key});
 
   @override
   State<PlantDetail> createState() => _PlantDetailState();
 }
 
 class _PlantDetailState extends State<PlantDetail> {
-  @override
-  void initState() {
-    super.initState();
+  void goToAI() {
+    Get.to(
+      () => const AIChatScreen(
+        aiChatType: AiChatType.detail,
+      ),
+      transition: Transition.rightToLeft,
+    );
+  }
+
+  void goToEdit() {
+    Get.off(() => const EditPlantForm(), transition: Transition.rightToLeft);
   }
 
   @override
   Widget build(BuildContext context) {
     final SelectedPlantDetailController selectedPlantDetailController =
         Get.find<SelectedPlantDetailController>();
+    PlantDetailController plantController = Get.put(PlantDetailController());
+    plantController.getPlantDetail();
 
-    PlantDetailController plantController = Get.find<PlantDetailController>();
-
-    return Scaffold(
+    return Obx(() {
+      return Scaffold(
         backgroundColor: BgColor.mainColor,
         appBar: CustomAppBar(
             title: selectedPlantDetailController.selectedPlant.nickName ?? "",
             rightComponent: plantController.plantDetail.value?.isMine == true
                 ? InkWell(
+                    onTap: goToEdit,
                     child: const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: Icon(Icons.create, color: Colors.black),
+                    ))
+                : ReportButton(
+                    reportType: ReportType.plant,
+                  )),
+        body: plantController.isLoading.value == true
+            ? Center(child: Lottie.asset('assets/lotties/loading_lottie.json'))
+            : Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                ),
+                child: ListView(
+                  children: [
+                    const SizedBox(
+                      height: 20,
                     ),
-                    onTap: () => {
-                          Get.off(const EditPlantForm(),
-                              transition: Transition.rightToLeft),
-                        })
-                : ReportButton()),
-        body: Obx(() {
-          if (plantController.isLoading.value == true) {
-            return Center(
-                child: Lottie.asset('assets/lotties/loading_lottie.json'));
-          } else {
-            return Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 15.0,
+                    ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20.0)),
+                        child: CustomImage(
+                            imgUrl: plantController.plantDetail.value?.imgUrl)),
+                    DetailInfoContainer(
+                      nickName: plantController.plantDetail.value?.nickName !=
+                              null
+                          ? "${plantController.plantDetail.value?.nickName}"
+                          : "${selectedPlantDetailController.selectedPlant.nickName}",
+                      scientificName: plantController
+                                  .plantDetail.value?.scientificName !=
+                              null
+                          ? "${plantController.plantDetail.value?.scientificName}"
+                          : "${selectedPlantDetailController.selectedPlant.scientificName}",
+                      period: plantController.plantDetail.value?.period != null
+                          ? plantController.plantDetail.value!.period
+                          : 0,
+                      heart:
+                          plantController.plantDetail.value?.heartCount != null
+                              ? plantController.plantDetail.value!.heartCount
+                              : 0,
+                      isMine: plantController.plantDetail.value?.isMine != null
+                          ? plantController.plantDetail.value!.isMine
+                          : false,
+                    ),
+                    const CustomCalendar(),
+                    const SizedBox(height: 30)
+                  ],
+                ),
               ),
-              child: ListView(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ClipRRect(
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(20.0)),
-                      child: CustomImage(
-                          imgUrl: plantController.plantDetail.value?.imgUrl)),
-                  DetailInfoContainer(
-                    nickName: plantController.plantDetail.value?.nickName !=
-                            null
-                        ? "${plantController.plantDetail.value?.nickName}"
-                        : "${selectedPlantDetailController.selectedPlant.nickName}",
-                    scientificName: plantController
-                                .plantDetail.value?.scientificName !=
-                            null
-                        ? "${plantController.plantDetail.value?.scientificName}"
-                        : "${selectedPlantDetailController.selectedPlant.scientificName}",
-                    period: plantController.plantDetail.value?.period != null
-                        ? plantController.plantDetail.value!.period
-                        : 0,
-                    heart: plantController.plantDetail.value?.heartCount != null
-                        ? plantController.plantDetail.value!.heartCount
-                        : 0,
-                    isMine: plantController.plantDetail.value?.isMine != null
-                        ? plantController.plantDetail.value!.isMine
-                        : false,
-                  ),
-                  const CustomCalendar(),
-                  const SizedBox(height: 30)
-                ],
-              ),
-            );
-          }
-        }));
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => goToAI(),
+          tooltip: 'talkWithAI',
+          backgroundColor: ColorStyles.mainAccent,
+          child: const Icon(Icons.messenger),
+        ),
+      );
+    });
   }
 }
