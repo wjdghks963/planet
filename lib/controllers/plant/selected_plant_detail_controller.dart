98e0f56a4baee69469e5de59106bc65bbf1f324a
wjdghks963 <chsw000@naver.com>
2023-11-29 13:20:54 +0900

    refactor : - 서비스 삭제하고 controller 에서 직접 조작

--- lib/controllers/plant/selected_plant_detail_controller.dart
+++ lib/controllers/plant/selected_plant_detail_controller.dart
@@ -1,10 +1,7 @@
 import 'package:get/get.dart';
 import 'package:planet/models/selected_plant_detail_model.dart';
-import 'package:planet/services/selected_plant_detail_service.dart';
 
 class SelectedPlantDetailController extends GetxController {
-  SelectedPlantDetailService service = Get.put(SelectedPlantDetailService());
-
   final Rx<SelectedPlantDetailModel> _selectedPlant =
       SelectedPlantDetailModel().obs;
 
@@ -18,14 +15,14 @@ class SelectedPlantDetailController extends GetxController {
       int? heartCount,
       bool? hearted,
       String? createdAt}) {
-    SelectedPlantDetailModel model = service.selectedDetail(
+    SelectedPlantDetailModel model = SelectedPlantDetailModel(
         plantId: plantId,
         nickName: nickName,
         imgUrl: imgUrl,
         scientificName: scientificName,
         heartCount: heartCount,
         hearted: hearted,
-        createdAt:createdAt);
+        createdAt: createdAt);
 
     _selectedPlant(model);
   }
